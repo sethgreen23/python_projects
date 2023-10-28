a85bc44eb2806c72d9dde0c993bc724715749cd1
Dridi Chaith <dridi.chaith@gmail.com>
2023-10-28 18:31:52 +0100

    word dictionary: make changes

--- 13_word_dictionary/word_dictionary.py
+++ 13_word_dictionary/word_dictionary.py
@@ -2,23 +2,27 @@ from PyDictionary import PyDictionary
 
 def word_meaning():
     dictionary = PyDictionary()
-    newLine = '\n*'
+    newLine = '\n* '
     while True:
         
         user_input = input("Please Enter a word to find meaning: ")
+        print()
         try:
             meaning = dictionary.meaning(user_input)
             for key, value in meaning.items():
-                print(f"{user_input}: '{key}'{newLine}{newLine.join(value)}")
+                print(f"{user_input.capitalize()}: '{key}'{newLine}{newLine.join(value)}")
+                if (len(meaning) > 1):
+                    print()
         except Exception as e:
             print(f"{user_input}: has no meaning.")
 
-        user_input = input("Do you want to have the meaning of another word (y/n)? ")
+        user_input = input("\nDo you want to have the meaning of another word (y/n)? ")
         if user_input.lower() == 'n':
-            print("Thank you for using our application.To the next time.")
+            print("\nThank you for using our application.To the next time.\n")
             break
         elif not user_input.lower() in 'yn':
             while not user_input.lower() in 'yn':
                 user_input = input("Please enter (y/n) to continue: ")
+        print()
 
 word_meaning()
