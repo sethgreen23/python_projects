94a725e72709849185fceca870a35c6aa0a83d64
Dridi Chaith <dridi.chaith@gmail.com>
2023-10-28 16:14:27 +0100

    leap year checker: make change

--- 12_leap_year_checker/leap_year_checker.py
+++ 12_leap_year_checker/leap_year_checker.py
@@ -8,12 +8,17 @@ def leap_year_checker():
         print(f"{year} is leap year.")
     else:
         print(f"{year} is not a leap year.")
-while True:
-    leap_year_checker()
+def check_user_input():
     user_input = input("Do you want to check another year? (y/n): ")
     while not user_input.lower() in'yn':
         user_input = input("Please choose between y/n: ")
         if user_input == 'n':
-            break
+            return -1
     if user_input.lower() == 'n':
-     break
+        return -1
+    return 1
+
+while True:
+    leap_year_checker()
+    if (check_user_input() == -1):
+        break
