afca6635e10a64c6cc5bec1fd3a1a7faf8fbf5ec
Dridi Chaith <dridi.chaith@gmail.com>
2023-12-10 18:37:27 +0100

    task11: value width and height

--- alx_projects/circular_python/models/square.py
+++ alx_projects/circular_python/models/square.py
@@ -25,3 +25,13 @@ class Square(Rectangle):
                                                          self.x,
                                                          self.y,
                                                          self.height)
+
+    @property
+    def size(self):
+        """Getter for the size value"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
