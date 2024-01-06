d3024fd62e5f7399bd2264a002a9ce598e80cf7b
root <root@DESKTOP-I3N2QEJ>
2024-01-06 17:20:34 +0100

    command arguments

--- alx_projects/airbnb_console/cmd/1-command_arguments.py
+++ alx_projects/airbnb_console/cmd/1-command_arguments.py
@@ -1,21 +1,22 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 import cmd
 
 class HelloWorld(cmd.Cmd):
-	
- 	def do_greet(self, person):
-      	"""greet [person] 
-     	Greet the names person
+    # change the prompt
+	prompt = ">>>"
+	def do_greet(self, person):
+		"""greet [person]
+		Greet the names person
 		"""
 		if person:
 			print("hi, ", person)
 		else:
-			print('hi')
+			print("hi")
+
 	def do_EOF(self, line):
-		print("Thank you for using our app")
+		print("Thank you for using our App.")
 		return True
 
 if __name__=="__main__":
-	HelloWorld().cmdloop()
-		
+    HelloWorld().cmdloop()
