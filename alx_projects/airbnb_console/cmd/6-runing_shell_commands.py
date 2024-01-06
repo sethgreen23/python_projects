76fc9d93913fc1e778d7f2aeca843352d4a661b6
Dridi Chaith <dridi.chaith@gmail.com>
2024-01-06 23:13:33 +0100

    runing shell commands

---
+++ alx_projects/airbnb_console/cmd/6-runing_shell_commands.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import cmd
+import subprocess
+
+class ShellEnabled(cmd.Cmd):
+    last_output = ''
+    
+    def do_shell(self, line):
+        """Run a shell command"""
+        print("running shell command: ", line)
+        sub_cmd = subprocess.Popen(line,
+                                   shell=True,
+                                   stdout=subprocess.PIPE)
+        output = sub_cmd.communicate()[0].decode('utf-8')
+        print(output)
+        self.last_output = output
+    
+    def do_echo(self, line):
+        """Print the input, replacing '$out' of the output
+        of the last command"""
+        print(line.replace('$out', self.last_output))
+    
+    def do_EOF(self, line):
+        return True
+
+if __name__=="__main__":
+    ShellEnabled().cmdloop()
