0af15cf6f2352e7bc96c2f17610d2eeeb873b6e3
Dridi Chaith <dridi.chaith@gmail.com>
2024-01-09 14:38:14 +0100

    indedation

--- alx_projects/airbnb_console/airbnb_project/models/base_model.py
+++ alx_projects/airbnb_console/airbnb_project/models/base_model.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python3
 """
-	Module representing the Base class of
-	all the classes
+    Module representing the Base class of
+    all the classes
 """
 from uuid import uuid4
 from datetime import datetime
 
+
 class BaseModel:
     """
-	BaseModel is a base class for all the future classes
-	
-	Args:
-		id(string): unique identifier for each instance
-		created_at(datetime): time when the instance is created
-		updated_at(datetime): time when the instance is updated
+    BaseModel is a base class for all the future classes
+
+    Args:
+        id(string): unique identifier for each instance
+        created_at(datetime): time when the instance is created
+        updated_at(datetime): time when the instance is updated
     """
-    
+
     def __init__(self):
         """Init function of the BaseModel class"""
         self.id = str(uuid4())
@@ -24,24 +25,23 @@ class BaseModel:
 
     def save(self):
         """Update the public instance attribute updated_at
-			with the current time
+            with the current time
         """
         self.updated_at = datetime.now()
-    
+
     def to_dict(self):
-        base_dict = {"__class__":self.__class__.__name__}
-        
+        """change the object to a disctionary"""
+        base_dict = {"__class__": self.__class__.__name__}
+
         for key, value in self.__dict__.items():
             if key in ["created_at", "updated_at"]:
                 base_dict[key] = value.isoformat()
             else:
                 base_dict[key] = value
         return base_dict
-    
+
     def __str__(self):
         """String representation of the class"""
         return "[{}] ({}) {}".format(self.__class__.__name__,
                                      self.id,
                                      self.__dict__)
-
-    
