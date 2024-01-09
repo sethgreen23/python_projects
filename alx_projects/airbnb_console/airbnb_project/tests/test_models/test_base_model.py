b68bc5f35b3b0c12a0214761d0c4cf09d9170bbe
Dridi Chaith <dridi.chaith@gmail.com>
2024-01-09 14:47:39 +0100

    fixing indetation on test

--- alx_projects/airbnb_console/airbnb_project/tests/test_models/test_base_model.py
+++ alx_projects/airbnb_console/airbnb_project/tests/test_models/test_base_model.py
@@ -5,11 +5,12 @@ import unittest
 from datetime import datetime
 from models.base_model import BaseModel
 
+
 class TestBaseModel(unittest.TestCase):
     """
-	Test class for BaseModel class
-    """       
-    
+    Test class for BaseModel class
+    """
+
     def test_uuid(self):
         """Testing the uuid"""
         b1 = BaseModel()
@@ -18,32 +19,32 @@ class TestBaseModel(unittest.TestCase):
         self.assertNotEqual(b1.id, b2.id)
         self.assertIsInstance(b1, BaseModel)
         self.assertIsInstance(b1.id, str)
-    
+
     def test_created_at(self):
         """Testing created_at"""
         b1 = BaseModel()
         self.assertTrue(hasattr(b1, "created_at"))
         self.assertIsInstance(b1.created_at, datetime)
-    
+
     def test_updated_at(self):
         """Testing updated_at"""
         b1 = BaseModel()
         self.assertTrue(hasattr(b1, "updated_at"))
         self.assertIsInstance(b1.updated_at, datetime)
-    
+
     def test_created_at_updated_at(self):
         """Testing dates"""
         b1 = BaseModel()
         b2 = BaseModel()
         self.assertNotEqual(b1.created_at, b2.created_at)
         self.assertNotEqual(b1.updated_at, b2.updated_at)
-    
+
     def test_save(self):
         """Testing save"""
         b1 = BaseModel()
         b1.save()
         self.assertIsInstance(b1.updated_at, datetime)
-    
+
     def test_to_dict(self):
         """Testing to_dict"""
         b1 = BaseModel()
@@ -52,7 +53,7 @@ class TestBaseModel(unittest.TestCase):
         self.assertEqual(d["__class__"], "BaseModel")
         self.assertIsInstance(d["created_at"], str)
         self.assertIsInstance(d["updated_at"], str)
-    
+
     def test_str(self):
         """Testing str"""
         b1 = BaseModel()
@@ -61,6 +62,7 @@ class TestBaseModel(unittest.TestCase):
         self.assertIn(b1.id, str_rep)
         # order preserving
         self.assertIn(str(b1.__dict__), str_rep)
-    
-if __name__=="__main__":
-    unittest.main()    
+
+
+if __name__ == "__main__":
+    unittest.main()
