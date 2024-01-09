49e897e34dea7400ce5e890570d776653bf56929
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-01-09 22:01:09 +0300

    Add all files

--- alx_projects/airbnb_console/airbnb_project/main3.py
+++ alx_projects/airbnb_console/airbnb_project/main3.py
@@ -17,9 +17,12 @@ for key in my_model_json.keys():
 print("--")
 my_new_model = BaseModel(**my_model_json)
 print(my_new_model.id)
+print("****************************")
 print(my_new_model)
 print(type(my_new_model.created_at))
 
 print("--")
 print(my_model is my_new_model)
 
+print("****************************")
+print
