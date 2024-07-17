5c580541dd0ebf30eea077dfc4bf67f9c2c3b51b
Dridi Chaith <dridi.chaith@gmail.com>
2024-07-17 14:01:32 +0100

    chapter 6

--- flask_apps/the_mega_flask_tutorial/microblog/app/forms.py
+++ flask_apps/the_mega_flask_tutorial/microblog/app/forms.py
@@ -2,12 +2,14 @@ from flask_wtf import FlaskForm
 from wtforms import (StringField,
                      PasswordField,
                      BooleanField,
-                     SubmitField
+                     SubmitField,
+                     TextAreaField
                      )
 from wtforms.validators import (DataRequired,
                                 ValidationError,
                                 Email,
-                                EqualTo)
+                                EqualTo,
+                                Length)
 import sqlalchemy as sa
 from app import db
 from app.models import User
@@ -41,3 +43,9 @@ class RegistrationForm(FlaskForm):
 		))
         if user is not None:
             raise ValidationError('Please use a different email address.')
+
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')
