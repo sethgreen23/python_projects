adb723eb7197f6be792c9ff83798dc12c4dc072f
Dridi Chaith <dridi.chaith@gmail.com>
2024-07-21 01:29:48 +0100

    chapter 8: followers

--- flask_apps/the_mega_flask_tutorial/microblog/app/forms.py
+++ flask_apps/the_mega_flask_tutorial/microblog/app/forms.py
@@ -61,3 +61,7 @@ class EditProfileForm(FlaskForm):
 			))
             if user is not None:
                 raise ValidationError('Please user a different username.')
+
+
+class EmptyForm(FlaskForm):
+    submit = SubmitField('Submit')
